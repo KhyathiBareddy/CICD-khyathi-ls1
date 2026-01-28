24f72bc3d2095059469234e1939ef4629393cc2d
MShiri9392 <Shiri957313@gmail.com>
2026-01-28 12:03:52 +0530

    updated

--- CICD/src/main/java/Evenodd.java
+++ CICD/src/main/java/Evenodd.java
@@ -5,11 +5,14 @@ public class Evenodd {
         Scanner input = new Scanner(System.in); 
         System.out.print("Enter an integer number: ");
         num = input.nextInt(); 
-        if (num % 2 == 0) {
+        if (num == 0) {
+            System.out.println("The given number is Zero.");
+        } else if (num % 2 == 0) {
             System.out.println("The given number " + num + " is Even.");
         } else {
             System.out.println("The given number " + num + " is Odd.");
         }
+        
 
         input.close(); 
     }
