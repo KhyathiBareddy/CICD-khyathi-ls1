d819789f27b622f991ffe2bc1302f4c0a853bc36
KhyathiBareddy <khyathi1369@gmail.com>
2026-03-03 22:50:31 +0530

    Update Evenodd.java

--- CICD/src/main/java/Evenodd.java
+++ CICD/src/main/java/Evenodd.java
@@ -11,8 +11,6 @@ public class Evenodd {
             System.out.println("The given number " + num + " is Even.");
         } else {
             System.out.println("The given number " + num + " is Odd.");
-        }else{
-            System.out.println("The given number does no exist.");
         }
         }
         
