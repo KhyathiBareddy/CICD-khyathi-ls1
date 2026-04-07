5fafc0ff9a7b3fe740678e66997e7e983df898e5
KhyathiBareddy <khyathi1369@gmail.com>
2026-04-07 14:25:49 +0530

    Update Evenodd.java

--- CICD/src/main/java/Evenodd.java
+++ CICD/src/main/java/Evenodd.java
@@ -1,24 +1,21 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class Evenodd {
     public static void main(String[] args) {
         int num;
-        Scanner input = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in);
+
         System.out.print("Enter an integer number: ");
-        num = input.nextInt(); 
+        num = input.nextInt();
+
         if (num == 0) {
             System.out.println("The given number is Zero.");
         } else if (num % 2 == 0) {
             System.out.println("The given number " + num + " is Even.");
         } else {
             System.out.println("The given number " + num + " is Odd.");
-        }else{
-            System.out.println("The given number does no exist.");
         }
-        }
-        
 
-        input.close(); 
+        input.close();
     }
 }
-
-
